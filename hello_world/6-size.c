238fc700539fc2e8583bdd516696f6a54fff7f32
alejandr088 <alejandrivello@gmail.com>
2023-02-15 14:32:28 -0800

    automatico

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(void)
+/* Este programa muestra el tamaño en bytes de varios tipos de datos en C */
 {
-        int i;
-        char c;
-        long int li;
-        long long int lli;
-        float f;
-
-
-        printf("Size of a char: %lu byte(s)\n", sizeof(c));
-        printf("Size of an int: %lu byte(s)\n", sizeof(i));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
-        printf("Size of a float: %lu byte(s)\n", sizeof(f));
-
-        return(0);
+int i;
+char c;
+long int li;
+long long int lli;
+float f;
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+return (0);
 }
