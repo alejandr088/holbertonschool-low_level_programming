1e26e59a0b4e544911a6b26c14c8f4ac9d28778a
alejandr088 <alejandrivello@gmail.com>
2023-02-16 05:56:34 -0800

    autogit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	ldigit = n % 10;
 	printf("last digit of %d is %d", n, ldigit);
 	if (n > 5)
-		printf("%d and is greater than 5", n);
+		printf("and is greater than 5");
 	else if (n == 0)
-		printf("%d and is 0", n);
+		printf("and is 0");
 	else if (n < 6 && n != 0)
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("and is less than 6 and not 0\n");
 
 	/* your code goes there */
 	return (0);
