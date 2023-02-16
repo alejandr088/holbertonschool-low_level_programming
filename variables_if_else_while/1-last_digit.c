2a3e561212c98fc3d1631baace91e5bbdc38bd6f
alejandr088 <alejandrivello@gmail.com>
2023-02-16 05:52:23 -0800

    autogit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ldigit = n % 10;
-	printf("last digit of %d is %d", n, ldigit);
+	printf("last digit of %d is %f", n, ldigit);
 	if (n > 5)
 		printf("%d and is greater than 5", n);
 	else if (n == 0)
