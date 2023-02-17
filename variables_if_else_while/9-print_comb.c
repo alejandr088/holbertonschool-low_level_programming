fbdceab6bfbd1c888b5f186d2dbcb48cab1220b8
alejandr088 <alejandrivello@gmail.com>
2023-02-17 04:23:48 -0800

    autogit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+int main(void)
+{
+int combis;
+
+for (combis = 0; combis <= 9; combis++)
+{
+putchar(combis + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
