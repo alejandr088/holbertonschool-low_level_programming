91e987f7c2a0144053684ddf820e02a6216c301b
alejandr088 <alejandrivello@gmail.com>
2023-02-19 14:49:22 -0800

    Autogit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -17,18 +17,18 @@ int print_sign(int n)
 {
 if (n >= 0)
 {
-putchar('+');
 return (1);
+putchar('+');
 }
 else
 if (n == 0)
 {
-_putchar('0');
+putchar('0');
 return (0);
 }
 else
 {
-_putchar('-');
+putchar('-');
 return (-1);
 }
 }
