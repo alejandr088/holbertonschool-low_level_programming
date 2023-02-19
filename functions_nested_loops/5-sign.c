41f789ff4254cfaf3d41ca1de8571541f2c69162
alejandr088 <alejandrivello@gmail.com>
2023-02-19 14:31:20 -0800

    Autogit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/*
+ *  *  * File: 5-sign.c
+ *   *   * Auth: alejandr088
+ *    *    *
+ *    **/
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  *  * print_sign - imprime el signo de un numero
+ *   *   * @n: single number input
+ *    *    * Return: 1 si n > 0, 0 si n es 0, y -1 si n < 1
+ *
+ */
+int print_sign(int n)
+{
+if (n >= 0)
+return (1);
+else
+if (n == 0)
+return (0);
+else
+if (n < 0)
+return (-1);
+}
