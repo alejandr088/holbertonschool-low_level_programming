497425899ec18e3eee80b79bf506e5e8e3f89b99
alejandr088 <alejandrivello@gmail.com>
2023-02-19 13:41:38 -0800

    Autogit

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 /*
- *  * File: 3-islower.c
- *   * Auth: alejandr088
- *    *
- *     */
+ * File: 3-islower.c
+ * Auth: alejandr088
+ *
+ */
 
 #include <stdio.h>
 #include "main.h"
 
 /**
- *  * int_islower(int c); - chequea caracteres en minuscula
- *   *
- *    * Return: 1 si esta en minusculas, de lo contrario 0
- *     */
+ * int _islower(int c); - chequea caracteres en minuscula
+ *
+ * Return: 1 si esta en minusculas, de lo contrario 0
+ */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
