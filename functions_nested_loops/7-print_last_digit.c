c751da0cce34fb2c77b5b905d88087a84a75bf6c
alejandr088 <alejandrivello@gmail.com>
2023-02-19 16:26:39 -0800

    Autogit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,28 +1,20 @@
-/**
- * File: 7-print_last_digit.c
- * Author: alejandr088
- * @n: int type number
- * * Return: return value of last digit
- */
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- *   * print_last_digit - Prints the last digit of a number
- *     * @n: The number to be treated
- *       *
- *         * Return: Value of the last digit of number
- *           */
-
-int print_last_digit(int n)
+ *  * print_last_digit - imprime el ultimo digito de un entero
+ *   * @num: ~
+ *    * Return: valor del ultimo numero
+ */
+int print_last_digit(int num)
 {
-int udigito;
+		int udigito;
 
-udigito = (n % 10);
+			udigito = (num % 10);
 if (udigito < 0)
 {
 udigito = (-1 * udigito);
 }
+
 _putchar(udigito + '0');
 return (udigito);
 }
