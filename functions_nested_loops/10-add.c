8f98a7f65423050d702374e7dfc4a17cc71825c8
alejandr088 <alejandrivello@gmail.com>
2023-02-19 17:31:06 -0800

    Autogit

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - agrega 2 enteros y devuelve
+ * @a: int a agregar a b
+ * @b: int a agregar a a
+ * Return: ~
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,11 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int add(int, int);
+	return (0);
+}
