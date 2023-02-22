15b19ba873ade2cf1f8caae547c5bbe40257b38a
alejandr088 <alejandrivello@gmail.com>
2023-02-22 09:09:48 -0800

    Autogit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - check code
+ * @a: single character input
+ * @b: single character input
+ * Return: 1 si esta en minusculas, de lo contrario 0
+ */
+int mul(int a, int b)
+{
+int mul = a * b;
+return (mul);
+}
