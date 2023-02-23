74f63e37001c704a177df0bb1c0a2fa2ae65edcf
alejandr088 <alejandrivello@gmail.com>
2023-02-23 09:04:59 -0800

    Autogit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,17 @@
 void more_numbers(void)
 {
 int num = 0;
-int num2 = 10;
+// int num2 = 10;
 int diezveces = 0;
 while (diezveces++ <= 9)
 {
-for (num = 0; num <= 14; num++)
+for (num = 0; num <= 9; num++)
 {
 putchar(num + '0');
-for (num2 = 10; num2 <= 14; num2++)
-{
-putchar(num2 + '0');
+// for (num2 = 10; num2 <= 14; num2++)
+// {
+// putchar(num2 + '0');
 }
 putchar('\n');
-}
 }
 }
