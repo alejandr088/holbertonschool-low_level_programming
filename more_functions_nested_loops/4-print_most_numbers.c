15727593e76d97900fdeb09836b877ef484b0b64
alejandr088 <alejandrivello@gmail.com>
2023-02-23 06:07:57 -0800

    Autogit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check code
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+int num;
+while ((num = 0 && num <= 9) && (num != 2 && num != 4))
+{
+num++;
+putchar(num + '0');
+}
+putchar('\n');
+}
