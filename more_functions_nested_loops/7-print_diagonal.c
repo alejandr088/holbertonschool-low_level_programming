c647e5e4557472f153e2aa87ee8051aebedbad55
alejandr088 <alejandrivello@gmail.com>
2023-02-24 07:23:41 -0800

    Autogit

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal - check code
+ * @n: single character
+ */
+void print_diagonal(int n)
+{
+	int a;
+for (a = 1; a <= n; a++)
+{
+putchar('\\');
+}
+putchar('\n');
+}
