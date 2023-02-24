0165d7e35f3d297e573e9ffc89a3b1411245fb50
alejandr088 <alejandrivello@gmail.com>
2023-02-24 13:52:47 -0800

    Autogit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * exercise - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+int exercise(void)
+{
+int nums;
+
+for (nums = 1; nums <= 100 ; nums++)
+{
+printf("%d", nums);
+if (nums % 3)
+{
+printf("Buzz ");
+if (nums % 5)
+{
+printf("Fizz ");
+}
+}
+else
+if (nums % 3 && nums % 5)
+{
+printf("BuzzFizz ");
+printf(" ");
+}
+printf("\n");
+}
+return (0);
+}
