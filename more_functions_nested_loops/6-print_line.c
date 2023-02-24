3422873fd7848f2d49a19326e10c95c1a45d4a0e
alejandr088 <alejandrivello@gmail.com>
2023-02-24 07:13:26 -0800

    Autogit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - check code
+ * @n: single character
+ */
+void print_line(int n)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar('_' + '0');
+}
+putchar('\n');
+}
