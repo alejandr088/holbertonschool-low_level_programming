f6ec66c5ad1e26f725253e07b13aec9af44484cb
alejandr088 <alejandrivello@gmail.com>
2023-02-27 07:20:52 -0800

    Autogit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * reset_to_98 - check code
+ * @n: single character input
+ * Return: 1 si s = int,, de lo contrario 0
+ */
+void reset_to_98(int *n)
+{
+int m = 98;
+
+printf("%d", *n = m);
+}
