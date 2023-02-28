019d47d20cb4d384f785d20b987440c7ead596dd
alejandr088 <alejandrivello@gmail.com>
2023-02-28 06:07:05 -0800

    Autogit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - check code
+ * @s: single string
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int len = 0;
+
+while (s[len] != 0)
+{
+len++;
+while (len > 0)
+{
+len--;
+putchar(s[len]);
+}
+putchar('\n');
+}
+}
