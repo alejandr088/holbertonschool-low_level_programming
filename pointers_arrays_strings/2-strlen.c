afb9880124d962f49f2b25b748d6c8a65b995df3
alejandr088 <alejandrivello@gmail.com>
2023-02-28 04:47:38 -0800

    Autogit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - check code
+ * @s: single character
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+s = "Holberton";
+int var1 = _strlen(s);
+
+return (var1);
+}
