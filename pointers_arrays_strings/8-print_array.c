735d233999de0a86b603a1d6112b5eb2bc504517
alejandr088 <alejandrivello@gmail.com>
2023-03-01 05:49:19 -0800

    Autogit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - check code
+ * @n: a int.
+ * @a: another int.
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+{
+printf("; ");
+}
+}
+
+printf('\n');
+}
