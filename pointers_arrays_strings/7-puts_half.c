4785dc250810cf40be643a6cdb243453c73f44ab
alejandr088 <alejandrivello@gmail.com>
2023-03-01 05:15:54 -0800

    Autogit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - check code
+ * @str: a string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int len = 0, i, j = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+}
+else if (len % 2)
+{
+for (i = (len - 1) / 2; j < len - 1; j++)
+{
+putchar(str[j - len]);
+}
+putchar('\n');
+}
+}
