788fd5f550d6baeaa4647078e406044042cbdcb8
alejandr088 <alejandrivello@gmail.com>
2023-03-02 06:04:29 -0800

    Autogit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcat - check code
+ * @dest: destiny
+ * @src: source file
+ * Return: pointer to "dest"
+ */
+char *_strcat(char *dest, char *src)
+{
+dest = src + '\0';
+return (dest);
+}
