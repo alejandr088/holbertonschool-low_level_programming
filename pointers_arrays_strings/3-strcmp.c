2c2793d3aab5369155f9ce3eb31e22e094374dea
alejandr088 <alejandrivello@gmail.com>
2023-03-03 04:41:00 -0800

    Autogit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,17 +3,19 @@
  * _strcmp - check code
  * @s1: first string
  * @s2: snd string
- * Return: "
+ * Return: n or difference btwn s1 & s2
  */
 int _strcmp(char *s1, char *s2)
 {
-int n = s1;
+int n;
 
 while (s1[n] == s2[n])
 {
 if (s1[n] == '\0')
+{
 return (n);
 }
 n++;
 }
 return (s1[n] - s2[n]);
+}
