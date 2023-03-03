57d6bf3aa8f36f3c4bd3f705f200490fe6e06630
alejandr088 <alejandrivello@gmail.com>
2023-03-03 05:41:35 -0800

    Autogit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,9 +8,9 @@ char *cap_string(char *str)
 {
 int i = 0;
 
-while (str[i])
+while (str[i] != '\0')
 {
-if (str[i] >= 'a' && str[i] <= 'z' && str[i] != '\0')
+if (str[i] >= 'a' && str[i] <= 'z')
 str[i] -= 32;
 i++;
 }
