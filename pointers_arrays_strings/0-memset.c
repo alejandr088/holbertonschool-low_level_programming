0dd59c0ace7c9765cd74a2f447726de93a81fd1d
alejandr088 <alejandrivello@gmail.com>
2023-03-05 12:50:07 -0800

    Autogit

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - Check code
+ * @s: pointer to s.
+ * @n: number
+ * @b: byte
+ * Return: the s value.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+for (int i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * leet - check code
+ * @a: string
+ * Return: 
+ */
+int main(void)
+{
+int i = 0;
+int len;
+for (i = 0; i <= len; i++)
+{
+if (str[i] = 'a' || str[i] = 'e' || str[i] = 'o' || str[i] = 't' || str[i] = 'l')
+
+}
