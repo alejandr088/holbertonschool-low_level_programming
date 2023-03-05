47742dc0bc9eaa125262d8bcd7a9d1f06caf3147
alejandr088 <alejandrivello@gmail.com>
2023-03-05 15:15:49 -0800

    Autogit

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard - Check code
+ * @a: pointer to s.
+ * Return: a pointer to byte in s, or NULL.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+char c;
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+c = *(a[i] + j);
+_putchar(c);
+_putchar(' ');
+}
+putchar('\n');
+}
+}
