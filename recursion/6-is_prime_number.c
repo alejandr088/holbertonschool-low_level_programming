93476590c80c83d854886e66f8da88caf14e0719
alejandr088 <alejandrivello@gmail.com>
2023-03-07 08:46:44 -0800

    Autogit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,10 +10,11 @@ int is_prime_number(int n)
 	if (n <= 1)
 	return (0);
 {
-	int i;
+	int i = 2;
 
-	for (i = 2; i * i <= n; i++)
+	if (i * i <= n)
 	{
+		i++;
 		if (n % i == 0)
 		{
 			return (0);
