816d37ce29bc103d28a45d55fa962541123228d7
alejandr088 <alejandrivello@gmail.com>
2023-03-07 08:45:25 -0800

    Autogit

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * is_prime_number - check the code
+ * @n: the number to check
+ * Return: the prime number if it is, else 0.
+ *
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	return (0);
+{
+	int i;
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+}
