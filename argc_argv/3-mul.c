c3eb981a14ce65f306e5f8fbb364f421552637d5
alejandr088 <alejandr088@github.com>
2023-03-12 12:26:08 -0700

    Autogit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - main function.
- *
- * @argc: count argument.
- * @argv: arrray argument
- *
- * Return: Always 0, 1 if error.
+ * main - main function
+ * @argc: count argument
+ * @argv: array argument
+ * Return: 0 if OK, 1 if error.
  */
-int main(int argc, char **argv)
-	{
-	 int result;
 
-	if (argc < 2)
-	{
+int main(int argc, char **argv)
+{
+int a = 0, b = 0;
+
+if (argc >= 2)
+{
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+}
+	else if (argc < 2)
+{
 		printf("Error\n");
 		return (1);
-	}
-	result = (int) *argv[1] * (int) *argv[2];
-	printf("%d\n", result);
-	return (0);
-	}
+		}
+return (0);
+}
