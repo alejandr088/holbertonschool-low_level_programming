ddbdff61740508d53bdbda72a895a7e6cf031a53
alejandr088 <alejandr088@github.com>
2023-03-12 12:05:15 -0700

    Autogit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,13 +13,10 @@ int main(int argc, char **argv)
 {
 	int result;
 
-	if (argc == 2)
-	{
 		result = (int) *argv[1] * (int) *argv[2];
 		printf("%d\n", result);
 	return (0);
-	}
-	else if (argc < 2)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
