ad79192df7ee12754384e110e0e4a624c36d1119
alejandr088 <alejandr088@github.com>
2023-03-26 08:27:22 -0700

    Autogit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef _DOG_H
 #define _DOG_H
-#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - a struct that represents a dog.
  * @name: name of the dog.
@@ -10,8 +12,10 @@
 
 struct dog
 {
-char *name; /**< Name of the dog */
-float age; /**< Age of the dog */
-char *owner; /**< Owner name of the dog */
+	char *name; /**< Name of the dog */
+	float age; /**< Age of the dog */
+	char *owner; /**< Owner name of the dog */
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
