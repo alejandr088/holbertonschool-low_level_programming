c3e196fdf5b0f1427fdb965c4464d7d9c57a31e0
alejandr088 <alejandr088@github.com>
2023-03-26 12:32:58 -0700

    Autogit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,10 +15,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	if (name == NULL || owner == NULL)
+	if (name == NULL)
 	{
 		free(p);
 		free(name);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
 		free(owner);
 		return (NULL);
 	}
