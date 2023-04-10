1c287d2d9458340d8fb276183c15bd10c8fc69d8
alejandr088 <alejandr088@github.com>
2023-04-10 13:32:30 -0700

    fixed finally in main.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -5,16 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * @n: int stored in node.
- * @prev: pointer to prev element of list.
- * @next: point to next element.
- *
- * Description: double linked lists node struct
- */
-
 unsigned int binary_to_uint(const char *b);
-
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
