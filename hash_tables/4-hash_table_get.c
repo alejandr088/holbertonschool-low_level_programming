8dd89ad8fca71b8ec845a49793282ea636dfc93e
alejandr088 <alejandr088@github.com>
2023-04-16 13:34:36 -0700

    funct 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,1 +1,30 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_get - Return a value associated with a key in a hash table
+ * @ht: hash table to look into
+ * @key: key
+ *
+ * Return: value associated with the key, or NULL if fail.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
+
