b2086e5581719d4ceed8d90230c8b36628569eef
alejandr088 <alejandr088@github.com>
2023-04-16 11:51:45 -0700

    fixed code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,13 +10,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *new_node = NULL, *temp = NULL;
+
 	if (!ht || !key || *key == '\0')
 		return (0);
 
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-
-;
-	hash_node_t *new_node, *temp;
 
 	temp = ht->array[index];
 	while (temp != NULL)
