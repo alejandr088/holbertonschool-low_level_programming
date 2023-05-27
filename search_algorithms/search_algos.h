541aa3c1b3b660e9517e213ded4a530ecb419b69
alejandr088 <alejandrivello@gmail.com>
2023-05-26 23:19:03 -0700

    fixed header

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,9 +1,9 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /* function prototypes */
 int linear_search(int *array, size_t size, int value);
